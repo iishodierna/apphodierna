cf83abc66abd4690ce6c2983e91a6b0a36892159
Mario Canalella <marcanalella@gmail.com>
2023-12-05 19:51:03 +0100

    upgrade all packages and flutter

--- android/app/src/main/kotlin/com/example/iishodierna/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/iishodierna/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.iishodierna
+package it.edu.iishodierna
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -12,30 +12,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(title: Text("IIS Hodierna")),
-          body: WebView(
-            initialUrl:
-                "https://sites.google.com/iishodierna.edu.it/www-iishodierna-edu-it/home",
-            javascriptMode: JavascriptMode.unrestricted,
-/*            onWebViewCreated: (WebViewController webViewController) {
-              _controller.complete(webViewController);
-            },*/
-            navigationDelegate: (NavigationRequest request) {
-              if (request.url.startsWith(
-                  "https://sites.google.com/iishodierna.edu.it/www-iishodierna-edu-it/")) {
-                return NavigationDecision.navigate;
-              } else {
-                _launchURL(request.url);
-                return NavigationDecision.prevent;
-              }
-            },
-          )
-      ),
+          body: WebViewWidget(
+              controller: WebViewController()
+                ..setJavaScriptMode(JavaScriptMode.unrestricted)
+                ..setNavigationDelegate(
+                  NavigationDelegate(
+                    onNavigationRequest: (NavigationRequest request) {
+                      if (request.url.startsWith(
+                          "https://sites.google.com/iishodierna.edu.it/www-iishodierna-edu-it/")) {
+                        return NavigationDecision.navigate;
+                      } else {
+                        _launchURL(request.url);
+                        return NavigationDecision.prevent;
+                      }
+                    },
+                  ),
+                )
+                ..loadRequest(Uri.parse(
+                    'https://sites.google.com/iishodierna.edu.it/www-iishodierna-edu-it/home')))),
     );
   }
 
   _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
+    if (await canLaunchUrl(url as Uri)) {
+      await launchUrl(url as Uri);
     } else {
       throw 'Could not launch $url';
     }
